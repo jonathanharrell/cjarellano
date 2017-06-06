4431879066cdea7f582f830d67910e5e900871b8
Jonathan Harrell <jonathan@worldhqinc.com>
2017-06-05 21:24:03 -0500

    updating hiq and removing button important flags

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 const menuOpen = document.querySelector('.menu-mobile-open')
 
-menuOpen.addEventListener('click', () => {
-    document.querySelector('body').classList.add('menu-mobile-visible')
-})
+if (menuOpen) {
+    menuOpen.addEventListener('click', () => {
+        document.querySelector('body').classList.add('menu-mobile-visible')
+    })
+}
 
 const menuClose = document.querySelector('.menu-mobile-close')
 
-menuClose.addEventListener('click', () => {
-    document.querySelector('body').classList.remove('menu-mobile-visible')
-})
+if (menuClose) {
+    menuClose.addEventListener('click', () => {
+        document.querySelector('body').classList.remove('menu-mobile-visible')
+    })
+}
