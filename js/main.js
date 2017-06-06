875ef5d9eb36d66cd5d510b7121ef3ecfa6f1966
Jonathan Harrell <jonathan@worldhqinc.com>
2017-06-05 21:43:05 -0500

    home page hover

--- js/main.js
+++ js/main.js
@@ -13,3 +13,15 @@ if (menuClose) {
         document.querySelector('body').classList.remove('menu-mobile-visible')
     })
 }
+
+document.querySelectorAll('.home nav a').forEach(link => {
+    link.addEventListener('mouseover', event => {
+        const linkClass = event.target.className
+        document.querySelector('body').classList.add(`${linkClass}-hover`)
+    })
+
+    link.addEventListener('mouseout', event => {
+        const linkClass = event.target.className
+        document.querySelector('body').classList.remove(`${linkClass}-hover`)
+    })
+})
