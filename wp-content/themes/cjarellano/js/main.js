c2579b6a23c92f50bf68730a8ca2316f3092ca86
Jonathan Harrell <jonathan@worldhqinc.com>
2017-06-28 18:43:12 -0500

    reverting

--- wp-content/themes/cjarellano/js/main.js
+++ wp-content/themes/cjarellano/js/main.js
@@ -1,3 +1,4 @@
+import Video from './Video'
 import { isInViewport } from './helpers';
 
 // fade in
@@ -30,4 +31,101 @@ document.addEventListener('scroll', () => {
             siteFooter.classList.add('visible')
         }
     }
+})
+
+// mobile menu
+const menuOpen = document.querySelector('.menu-mobile-open')
+
+if (menuOpen) {
+    menuOpen.addEventListener('click', () => {
+        console.log('test')
+        document.querySelector('body').classList.add('menu-mobile-visible')
+    })
+}
+
+const menuClose = document.querySelector('.menu-mobile-close')
+
+if (menuClose) {
+    menuClose.addEventListener('click', () => {
+        document.querySelector('body').classList.remove('menu-mobile-visible')
+    })
+}
+
+// desktop menu hover
+const underline = document.querySelector('.underline')
+
+let startingPositionLeft
+let startingWidth
+let hoveringMenuItem = false
+
+const positionMenuUnderline = () => {
+    const currentMenuItem = document.querySelector('.current-menu-item')
+    if (currentMenuItem) {
+        startingPositionLeft = currentMenuItem.getBoundingClientRect().left
+        startingWidth = currentMenuItem.offsetWidth
+
+        underline.style.left = `${startingPositionLeft}px`
+        underline.style.width = `${startingWidth}px`
+
+        underline.classList.add('visible')
+    }
+}
+
+window.onload = () => {
+    positionMenuUnderline()
+}
+
+window.addEventListener('resize', () => {
+    positionMenuUnderline()
+})
+
+document.querySelectorAll('.primary-nav .menu-item').forEach(menuItem => {
+    menuItem.addEventListener('mouseover', event => {
+        hoveringMenuItem = true
+        underline.classList.add('visible')
+
+        const { target } = event
+        const positionLeft = target.getBoundingClientRect().left
+        const width = target.offsetWidth
+
+        underline.style.left = `${positionLeft}px`
+        underline.style.width = `${width}px`
+    })
+
+    menuItem.addEventListener('mouseout', event => {
+        hoveringMenuItem = false
+
+        setTimeout(() => {
+            if (!hoveringMenuItem) {
+                if (!event.relatedTarget.classList.contains('menu-item')) {
+                    if (startingPositionLeft && startingWidth) {
+                        underline.style.left = `${startingPositionLeft}px`
+                        underline.style.width = `${startingWidth}px`
+                    } else {
+                        underline.classList.remove('visible')
+                    }
+                }
+            }
+        }, 350)
+    })
+})
+
+// videos
+const videos = document.querySelectorAll('.watch-video')
+for (let i = 0; i < videos.length; i++) {
+    const video = new Video(videos[i])
+    video.init()
+}
+
+// home page hover
+document.querySelectorAll('.home nav a').forEach(link => {
+    link.addEventListener('mouseover', event => {
+        const linkClass = event.target.className
+        document.querySelector('body').classList.add(`${linkClass}-hover`)
+    })
+
+    link.addEventListener('mouseout', event => {
+        const linkClass = event.target.className
+        document.querySelector('body').classList.remove(`${linkClass}-hover`)
+    })
 })
