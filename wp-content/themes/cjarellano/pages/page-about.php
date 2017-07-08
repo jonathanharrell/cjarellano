be396e8b1fb3621f30eef174b97ac377ac1f702d
Jonathan Harrell <jonathan@worldhqinc.com>
2017-07-08 11:25:43 -0500

    about template

---
+++ wp-content/themes/cjarellano/pages/page-about.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: About
+ */
+
+the_post();
+get_header();
+?>
+
+<?php include '../partials/site-header.php'; ?>
+
+<div class="wrapper">
+    <main class="main-content">
+
+    </main>
+</div>
+
+<?php include '../partials/site-footer.php'; ?>
+
+<?php get_footer();
