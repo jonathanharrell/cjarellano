e4f5eac87a4119f90b55302dce3f1cd26710e6c6
Jonathan Harrell <jonathan@worldhqinc.com>
2017-07-08 19:08:56 -0500

    browser testing fixes

--- wp-content/themes/cjarellano/partials/content-site-footer.php
+++ wp-content/themes/cjarellano/partials/content-site-footer.php
@@ -1,8 +1,10 @@
 <footer class="site-footer transition-in" role="contentinfo">
-    <h2 class="contact-me">
-        <span>Contact Me</span>
-    </h2>
-    <a href="mailto:cj@cjarellano.com" class="email">cj@cjarellano.com</a>
-    <?php get_template_part( 'partials/content', 'social-links' ); ?>
-    <p class="copyright">&copy; 2017 | Site by <a href="">Jonathan Harrell</a></p>
+    <div class="container">
+        <h2 class="contact-me">
+            <span>Contact Me</span>
+        </h2>
+        <a href="mailto:cj@cjarellano.com" class="email">cj@cjarellano.com</a>
+        <?php get_template_part( 'partials/content', 'social-links' ); ?>
+        <p class="copyright">&copy; 2017 | Site by <a href="">Jonathan Harrell</a></p>
+    </div>
 </footer>
--- wp-content/themes/cjarellano/partials/content-site-header.php
+++ wp-content/themes/cjarellano/partials/content-site-header.php
@@ -1,19 +1,21 @@
 <header class="site-header transition-in">
-    <a href="<?php echo esc_url(home_url('/')); ?>" class="brand">C.J. Arellano</a>
-    <div class="logo">
-        <?php echo file_get_contents(get_template_directory_uri() . '/img/logo.svg'); ?>
+    <div class="site-header-content">
+        <a href="<?php echo esc_url(home_url('/')); ?>" class="brand">C.J. Arellano</a>
+        <div class="logo">
+            <?php echo file_get_contents(get_template_directory_uri() . '/img/logo.svg'); ?>
+        </div>
+        <nav class="primary-nav">
+            <?php
+            wp_nav_menu([
+                'theme_location' => 'primary_nav',
+                'container'      => false,
+                'items_wrap'     => '%3$s'
+            ]);
+            ?>
+            <a href="mailto:cj@cjarellano.com" class="link-email">
+                <?php echo file_get_contents(get_template_directory_uri() . '/img/email.svg'); ?>
+            </a>
+        </nav>
     </div>
-    <nav class="primary-nav">
-        <?php
-        wp_nav_menu([
-            'theme_location' => 'primary_nav',
-            'container'      => false,
-            'items_wrap'     => '%3$s'
-        ]);
-        ?>
-        <a href="mailto:cj@cjarellano.com" class="link-email">
-            <?php echo file_get_contents(get_template_directory_uri() . '/img/email.svg'); ?>
-        </a>
-    </nav>
     <span class="menu-mobile-background"></span>
 </header>
