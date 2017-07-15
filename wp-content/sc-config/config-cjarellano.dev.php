3efc1a65759fe1b22459f7c3f3a47e0fce11ae91
Jonathan Harrell <jonathan@worldhqinc.com>
2017-07-15 16:41:23 -0500

    removing cache

--- wp-content/sc-config/config-cjarellano.dev.php
+++
@@ -1,13 +0,0 @@
-<?php 
-defined( 'ABSPATH' ) || exit;
-return array (
-  'enable_page_caching' => true,
-  'advanced_mode' => false,
-  'enable_in_memory_object_caching' => false,
-  'enable_gzip_compression' => true,
-  'in_memory_cache' => 'memcached',
-  'page_cache_length' => 1440,
-  'cache_exception_urls' => '',
-  'enable_url_exemption_regex' => false,
-); 
-
